10c3a1ed4a350a1b06cfeb55f476478619ac41fa
nxvertime <nxvertime@gmail.com>
2021-07-21 21:26:19 +0200

    added usefull.py and updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from colorama import *
 from os import name, system, getenv
 import socket, os
 from time import sleep
-
+from usefull import *
 from optparse import OptionParser
 ## set the clear command by os name
 windows = False
@@ -20,40 +20,7 @@ else: ## linux distrib.
 ## init colorama to dodge the problems
 init()
 
-## set colors more simple :)
 
-red = Fore.RED
-lightRed = Fore.LIGHTRED_EX
-
-blue = Fore.BLUE
-lightBlue = Fore.LIGHTBLUE_EX
-
-green = Fore.GREEN
-lightGreen = Fore.LIGHTGREEN_EX
-
-yellow = Fore.YELLOW
-lightYellow = Fore.LIGHTYELLOW_EX
-
-magenta = Fore.MAGENTA
-lightMagenta = Fore.LIGHTMAGENTA_EX
-
-cyan = Fore.CYAN
-lightCyan = Fore.LIGHTCYAN_EX
-
-white = Fore.RESET
-
-black = Fore.BLACK
-
-reset = Fore.RESET
-
-## set usefull vars
-
-attention = f'{reset}[{yellow}!{reset}] '
-info = f'{reset}[{cyan}*{reset}] '
-ok = f'{reset}[{lightGreen}ok{reset}] '
-success = f'{reset}[{green}v{reset}] '
-error = f'{reset}[{yellow}!{reset}] '
-question = f'{reset}[{lightGreen}?{reset}] '
 ## functions of modes
 def server():
     if windows:
